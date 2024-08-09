7932fa6bd34c6fef5954e54f3ad2e18599d6af8a
chandra-12-sinha <chandrakalasinha083@gmail.com>
2024-08-09 14:23:23 +0530

    import all files in app.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
+
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
+import Navbar from './Components/Navbar/Navbar'
+import Home from './Components/Home/Home'
+import About from './Components/About/About'
+import Help from './Components/Help/Help'
+import View from './Components/View/View'
+import Contact_us from './Components/Contact_Us/Contact_us'
 
 function App() {
+  const router = createBrowserRouter(createRoutesFromElements(
+    <Route path=',' element={<Navbar/>}>
+      <Route index element={<Home/>}/>
+      <Route index element={<About/>}/>
+    <Route path='Help' element={<Help/>}>
+    <Route path='View' element={<View/>}/>
+    <Route path='Cantact_Us' element={<Contact_us/>}/>
+
+    </Route>
+    </Route>
+  ))
   return (
-   
+   <RouterProvider router={router}/>
   )
 }
 
